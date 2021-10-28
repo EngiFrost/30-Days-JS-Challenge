36a5946aca089a613cca0cdd86ac8e99d51d0d11
Daniel Frost <flint.bets@yandex.ru>
2021-10-28 22:28:25 +0300

    Added key animations and fixed styles

--- Day 1. Drum Kit/drumkit.js
+++ Day 1. Drum Kit/drumkit.js
@@ -1,7 +1,14 @@
 window.addEventListener('keydown', (event) => {
   const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
 
-  if (!audio) return; // do nothing if key has no sound
+  if (!audio) return; // do nothing if the key has no sound
 
+  audio.currentTime = 0; //rewind the start
   audio.play();
+
+  key.classList.add('playing');
+  setTimeout(() => {
+    key.classList.remove('playing');
+  }, 100);
 });
