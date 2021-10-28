abc442d150a06f13cf728cc3d8cb0f724ac13f24
Daniel Frost <flint.bets@yandex.ru>
2021-10-28 21:44:03 +0300

    day-1 squash commit

---
+++ Day 1. Drum Kit/drumkit.js
@@ -0,0 +1,26 @@
+window.addEventListener('keydown', playSound);
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => {
+  key.addEventListener('transitionend', removeTransition);
+});
+
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+
+  if (!audio) return; // do nothing if the key has no sound
+
+  audio.currentTime = 0; //rewind the start
+  audio.play();
+
+  key.classList.add('playing');
+  // setTimeout(() => {
+  //   key.classList.remove('playing');
+  // }, 100); // it doesn't handle css transition!
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform') return; // skip if it is not a transform
+  this.classList.remove('playing'); // this equals to key because removeTransition is key's event handler
+}
--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## Sources
+[GitHub](https://github.com/wesbos/JavaScript30)
 [Site](https://www.javascript30.com/), 
 [YouTube](https://www.youtube.com/playlist?list=PLu8EoSxDXHP6CGK4YVJhL_VWetA865GOH)
+
+### Day 1. Drum kit
+
+[keycode.info](keycode.info)
+[Emmet](https://docs.emmet.io/abbreviations/syntax/)
+[Keyboard events](https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent)
