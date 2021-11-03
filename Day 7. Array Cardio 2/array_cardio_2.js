bc845ad6b5390f4e53d8bc3f2e48e951d000430a
Daniel Frost <flint.bets@yandex.ru>
2021-11-03 10:26:35 +0300

    Solved all tasks

--- Day 7. Array Cardio 2/array_cardio_2.js
+++ Day 7. Array Cardio 2/array_cardio_2.js
@@ -17,12 +17,31 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const isSomeAdult = people.some((person) => new Date().getFullYear() - person.year >= 19);
+console.log(isSomeAdult);
+
 // Array.prototype.every() // is everyone 19 or older?
+const isEveryAdult = people.every((person) => new Date().getFullYear() - person.year >= 19);
+console.log(isEveryAdult);
 
 // Array.prototype.find()
-// Find is like filter, but instead returns just the one you are looking for
+const findAdult = people.find((person) => new Date().getFullYear() - person.year >= 19);
+console.log(findAdult);
+
+// Find is like filter, but instead returns just the first one you are looking for
 // find the comment with the ID of 823423
+const findComment = comments.find((comment) => comment.id === 823423);
+console.log(findComment);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+
+const commentIdx = comments.findIndex((comment) => comment.id === 823423);
+console.log(commentIdx);
+comments.splice(commentIdx, 1);
+console.table(comments);
+
+// Another way to delete:
+const newComments = [...comments.slice(0, commentIdx), ...comments.slice(commentIdx)];
+console.table(newComments);
